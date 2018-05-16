5000146bd52304004e2e336331233aaeb763d2ac
Julian Boyko <julian.boyko@outlook.com>
2018-05-16 15:03:22 -0400

    Request Wordlist

--- WordOfTheDay/WordOfTheDay/NetworkManager.swift
+++ WordOfTheDay/WordOfTheDay/NetworkManager.swift
@@ -42,5 +42,29 @@ class NetworkManager: NSObject {
 
     }
     
+    public func requestWordList() {
+        let appId = "e054a255"
+        let appKey = "539a60447b7dd113f1d9eba18a132b93"
+        let language = "en"
+        let filters = "registers=Rare;domains=Art"
+        let url = URL(string: "https://od-api.oxforddictionaries.com:443/api/v1/wordlist/\(language)/\(filters)")!
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue(appId, forHTTPHeaderField: "app_id")
+        request.addValue(appKey, forHTTPHeaderField: "app_key")
+        
+        let session = URLSession.shared
+        _ = session.dataTask(with: request, completionHandler: { data, response, error in
+            if let response = response,
+                let data = data,
+                let jsonData = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) {
+                print(response)
+                print(jsonData)
+            } else {
+                print(error!)
+                print(NSString.init(data: data!, encoding: String.Encoding.utf8.rawValue)!)
+            }
+        }).resume()
+    }
     
 }
--- WordOfTheDay/WordOfTheDay/ViewController.swift
+++ WordOfTheDay/WordOfTheDay/ViewController.swift
@@ -22,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonClicked(_ sender: Any) {
+        NetworkManager().requestWordList();
+    }
 
 }
 
